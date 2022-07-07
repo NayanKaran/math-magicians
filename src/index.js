24b8037ff1c02b8d611f43fa1055c140b3502587
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-07 17:00:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import CalculatorPage from './pages/CalculatorPage';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
 
