079937efce354b2a03742ff4533954fb20cb6ae4
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-07 17:44:35 +0530

    Apply JavaScript best practices

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ import './index.css';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 import CalculatorPage from './pages/CalculatorPage';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/calculator" element={<CalculatorPage />} />
         <Route path="/quote" element={<Quote />} />
@@ -23,8 +22,3 @@ root.render(
     </Router>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
